13e39d752f581a83808347516d63f538f14c857c
Shubhangi Takawane <Shubhangi.Takawane@walmartlabs.com>
2018-10-03 10:22:37 -0700

    Simple Java program

--- src/Java1.java
+++ src/Java1.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Java1 {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Hello java");
-	}
+	    public static void main(String[] args) {
+	    	
+	        Scanner scan = new Scanner(System.in);
+	        
+	        System.out.println("enter the integer=");
+	        int i = scan.nextInt();
+	        System.out.println("enter the string=");
+	       String s =scan.nextLine();
+	       System.out.println("enter the double=");
+	       
+	      // Double d=scan.nextDouble();
+
+	        System.out.println("String: " + s);
+	      //  System.out.println("Double: " + d);
+	        System.out.println("Int: " + i);
+	    }
 
 }
